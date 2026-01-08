be8051d2f00b17d2e9d0b00347be983c3af707f1
Iman <imanghafoori1@gmail.com>
2026-01-08 04:27:36 +0330

    init

--- tests/Feature/CheckDynamicWhereTest.php
+++ tests/Feature/CheckDynamicWhereTest.php
@@ -1,13 +1,44 @@
 <?php
 
 use Illuminate\Foundation\Testing\TestCase;
+use Imanghafoori\ImportAnalyzer\ErrorReporters\ErrorPrinter;
+use Imanghafoori\LaravelMicroscope\Foundations\Reports\ComposerJsonReport;
 
 class CheckDynamicWhereTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        ComposerJsonReport::$callback = null;
+        copy(__DIR__.'/DynamicWhereStubs/dynamic-where-init.stub', $this->mainPath());
+    }
+
+    public function tearDown(): void
+    {
+        ErrorPrinter::$instance = null;
+        ComposerJsonReport::$callback = null;
+
+        @unlink($this->mainPath());
+        parent::tearDown();
+    }
+
     public function test()
     {
-        $r = $this->artisan('check:dynamic_wheres')->run();
+        $r = $this->artisan('check:dynamic_wheres')
+            ->expectsConfirmation('Do you want to replace dynamic_wheres.php with new version of it?', 'yes')
+            ->run();
 
         $this->assertEquals(0, $r);
+
+        $this->assertEquals(
+            file_get_contents(__DIR__.'/DynamicWhereStubs/dynamic-where-final.stub'),
+            file_get_contents($this->mainPath())
+        );
+
+    }
+
+    private function mainPath()
+    {
+        return app_path('dynamic_wheres.php');
     }
 }
--- tests/Feature/CheckGatesTest.php
+++ tests/Feature/CheckGatesTest.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Testing\TestCase;
 use Imanghafoori\LaravelMicroscope\ErrorReporters\ErrorPrinter;
+use Imanghafoori\LaravelMicroscope\Features\CheckGates\SpyGate;
 
 class CheckGatesTest extends TestCase
 {
@@ -9,6 +12,13 @@ class CheckGatesTest extends TestCase
     {
         ErrorPrinter::$instance = null;
 
+        SpyGate::start();
+        app(GateContract::class)->define('update', function ($user) {
+            return true;
+        });
+        app(GateContract::class)->define('update', 'f@f');
+        app(GateContract::class)->define('update', User::class);
+        app(GateContract::class)->define('update', User::class.'@s');
         $r = $this->artisan('check:gates')->run();
 
         $this->assertEquals(0, $r);
